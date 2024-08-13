4196162458fbfb4f1a1dcca2684b63d631a36fe1
JeffLeite91 <jefrey_jele@hotmail.com>
2024-08-12 21:30:16 -0300

    update

--- dev/pague-mais/src/components/Modal/modal.tsx
+++ dev/pague-mais/src/components/Modal/modal.tsx
@@ -7,52 +7,56 @@ export const Modal = () => {
   };
 
   return (
-    <> 
-        <div className="border-zinc-100">
-    <button
-        className="flex justify-end items-end border border-zinc-100 my-5 rounded p-2 bg-blue-500 text-zinc-100 font-semibold"
-        onClick={modal}
-      >
-        Adicionar item
-      </button>
-      {openModal && (
-        <div className="bg-blue-800 p-8 mb-4 rounded flex flex-col items-center justify-center fixed">
-          <input
-            className="flex text-center my-2"
-            type="text"
-            placeholder="Digite novo item"
-          />
-          <input
-            className="flex text-center my-2"
-            type="number"
-            placeholder="Digite o preço"
-          />
-          <input
-            className="flex text-center my-2 h-14"
-            type="text"
-            placeholder="Descrição"
-          />
-          <input
-            className="flex text-center my-2"
-            type="text"
-            placeholder="Qtd. estoque"
-          />
-          <div className="flex flex-row gap-5 my-2">
-            <button className="bg-blue-200 font-bold text-blue-800 rounded p-1">
-              Salvar
-            </button>
-            <button
-              className="bg-red-500 font-bold text-zinc-100 rounded p-1"
-              onClick={modal}
-            >
-              Cancelar
-            </button>
-          </div>
-        </div>
-      )}
+    <>
+      <div className="border-zinc-100 relative">
+        <button
+          className="flex justify-end items-end border border-zinc-100 my-5 rounded p-2 bg-blue-500 text-zinc-100 font-semibold"
+          onClick={modal}
+        >
+          Adicionar item
+        </button>
 
-    </div>
-     
+        {openModal && (
+          <>
+            {/* Overlay com efeito de desfoque */}
+            <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
+
+            <div className="bg-blue-800 p-8 mb-4 rounded flex flex-col items-center justify-center fixed z-10  m-auto h-1/2 max-w-72">
+              <input
+                className="flex text-center my-2"
+                type="text"
+                placeholder="Digite novo item"
+              />
+              <input
+                className="flex text-center my-2"
+                type="number"
+                placeholder="Digite o preço"
+              />
+              <input
+                className="flex text-center my-2 h-14"
+                type="text"
+                placeholder="Descrição"
+              />
+              <input
+                className="flex text-center my-2"
+                type="text"
+                placeholder="Qtd. estoque"
+              />
+              <div className="flex flex-row gap-5 my-2">
+                <button className="bg-blue-200 font-bold text-blue-800 rounded p-1">
+                  Salvar
+                </button>
+                <button
+                  className="bg-red-500 font-bold text-zinc-100 rounded p-1"
+                  onClick={modal}
+                >
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 };
